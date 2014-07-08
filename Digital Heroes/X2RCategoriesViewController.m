f69ce0b252ac4a6b1d07ad1b7f2aded46b80c652
Jose <jlrepresa@gmail.com>
2014-07-08 03:30:56 -0700

    Added flat buttons

--- Digital Heroes/X2RCategoriesViewController.m
+++ Digital Heroes/X2RCategoriesViewController.m
@@ -8,6 +8,7 @@
 
 #import "X2RCategoriesViewController.h"
 #import "NSString+FontAwesome.h"
+#import "FlatButton.h"
 
 @interface X2RCategoriesViewController ()
 
@@ -29,21 +30,35 @@
     [super viewDidLoad];
 	// Do any additional setup after loading the view.
     NSString *iconGithub = [NSString fontAwesomeIconStringForEnum:FAGithub];
+    UIFont *fontAwesome = [UIFont fontWithName:kFontAwesomeFamilyName size:20];
     
-    self.btnTest.titleLabel.font = [UIFont fontWithName:kFontAwesomeFamilyName size:20];
-    
+    self.btnTest.titleLabel.font = fontAwesome;
     [self.btnTest setTitle:[NSString stringWithFormat:@"%@", iconGithub] forState:UIControlStateNormal];
     
+    
     UILabel *test = [[UILabel alloc] initWithFrame:CGRectMake(0,5,100,20)];
-    test.font = [UIFont fontWithName:kFontAwesomeFamilyName size:20];
+    test.font = fontAwesome;
     test.text = [NSString stringWithFormat:@"%@", iconGithub];
     [self.view addSubview:test];
+    
+    
+    UIButton *flatBtn = [[FlatButton alloc] initWithFrame:CGRectMake(20, 350, 280, 40) withBackgroundColor:[UIColor greenColor]];
+    flatBtn.layer.cornerRadius = 10;
+    [flatBtn setTitle:iconGithub forState:UIControlStateNormal];
+    flatBtn.titleLabel.font = fontAwesome;
+    [flatBtn addTarget:self action:@selector(flatBtnPressed:) forControlEvents:UIControlEventTouchUpInside];
+    [self.view addSubview:flatBtn];
+    
 }
 
 - (void)didReceiveMemoryWarning
 {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+-(void)flatBtnPressed:(id)button{
+    NSLog(@"Flat btn pressed");
 }
 
 @end
